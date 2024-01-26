9b71a881fb36c981cf1452a826e083a66efdde73
Young Developer <ngochuy.mmt@gmail.com>
2024-01-26 14:05:31 +0700

    code cuar tao ok

---
+++ src/main/java/com/example/MockFjb05Nhom2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockFjb05Nhom2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockFjb05Nhom2Application.class, args);
+    }
+
+}
