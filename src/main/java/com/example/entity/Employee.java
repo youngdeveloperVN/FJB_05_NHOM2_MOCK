7121b6d96ccd281f024bfa6cede64fd7caae9e2f
Young Developer <ngochuy.mmt@gmail.com>
2024-01-26 15:13:49 +0700

    add chuc nang UP emplyoee

---
+++ src/main/java/com/example/entity/Employee.java
@@ -0,0 +1,16 @@
+package com.example.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+public class Employee {
+
+    String username;
+    String passsss;
+
+    BigDecimal salary;
+}
