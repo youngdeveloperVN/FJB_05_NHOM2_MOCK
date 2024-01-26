ae9261f3e5d171a048066573a2bb371bc1b0d6b4
Young Developer <ngochuy.mmt@gmail.com>
2024-01-26 15:16:46 +0700

    code UP xong

---
+++ src/main/java/com/example/entity/Employee.java
@@ -0,0 +1,17 @@
+package com.example.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Setter
+@Getter
+public class Employee {
+
+    String username;
+
+    String passsss;
+
+    BigDecimal salary;
+}
