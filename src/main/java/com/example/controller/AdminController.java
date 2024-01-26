506e1436ee540878044afdcb0268c5f928cb5d98
Young Developer <ngochuy.mmt@gmail.com>
2024-01-26 14:55:54 +0700

    chuc nang CRUD employe => làm dc chuc nang Create

--- src/main/java/com/example/controller/AdminController.java
+++ src/main/java/com/example/controller/AdminController.java
@@ -1,4 +1,20 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class AdminController {
+
+    //CRUD Employee
+    @GetMapping("/employee-create")
+    public String createEmployeeUI(){
+        return "admin/employee/create";
+    }
+
+    @PostMapping("/employee-create")
+    public String createEmployee(){
+        return "admin/employee/create";
+    }
+
+
 }
